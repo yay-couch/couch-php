7e518630a2d060a5e39629986e8abcd6670168ee
Kerem Gunes <qeremy@gmail.com>
2015-10-18 20:44:48 +0300

    Add getBody, getHeader methods.

--- Couch/Http/Stream.php
+++ Couch/Http/Stream.php
@@ -6,6 +6,15 @@ abstract class Stream
     protected $body;
     protected $headers = [];
 
+    public function getBody() {
+        return $this->body;
+    }
+
+    public function getHeader($key) {
+        return isset($this->headers[$key])
+            ? $this->headers[$key] : null;
+    }
+
     abstract public function setBody($body);
     abstract public function setHeader($key, $value);
 }
