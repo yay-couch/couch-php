f21572d8ccc3a7e4e34dc7cc0aa072bd1bf4a755
Kerem Gunes <qeremy@gmail.com>
2015-10-19 16:00:19 +0300

    Create Document class.

--- Couch/Object/Document.php
+++ Couch/Object/Document.php
@@ -1,7 +1,24 @@
 <?php
-namespace Couch;
+namespace Couch\Object;
 
 class Document
+    extends \Couch\Object
 {
     private $data = [];
+
+    public function __construct($client, array $data = null) {
+        parent::__construct($client);
+
+        if (!empty($data)) {
+            $this->setData($data);
+        }
+    }
+
+    public function setData(array $data) {
+        $this->data = $data;
+    }
+
+    public function getData() {
+        return $this->data;
+    }
 }
