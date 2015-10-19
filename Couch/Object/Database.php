206b4966f618d764218cb47b2c96622cc9f486e1
Kerem Gunes <qeremy@gmail.com>
2015-10-19 16:08:49 +0300

    Add Database methods.

--- Couch/Object/Database.php
+++ Couch/Object/Database.php
@@ -1,7 +1,46 @@
 <?php
-namespace Couch;
+namespace Couch\Object;
 
 class Database
+    extends \Couch\Object
 {
     private $name;
+
+    public function __construct($client, $name) {
+        parent::__construct($client);
+
+        $this->setName($name);
+    }
+
+    public function setName($name) {
+        $this->name = $name;
+    }
+    public function getName() {
+        return $this->name;
+    }
+
+    // http://docs.couchdb.org/en/1.5.1/api/database/common.html#head--{db}
+    public function ping() {
+        return (200 === $this->client->head('/')->getStatusCode());
+    }
+    // http://docs.couchdb.org/en/1.5.1/api/database/common.html#get--{db}
+    public function info($key = null) {
+        $info = $this->client->get('/'. $this->name)->getData();
+        return ($key && isset($info[$key]))
+            ? $info[$key] : $info;
+    }
+    // http://docs.couchdb.org/en/1.5.1/api/database/common.html#put--{db}
+    public function create() {
+        return (true === $this->client->put('/'. $this->name)->getData('ok'));
+    }
+    // http://docs.couchdb.org/en/1.5.1/api/database/common.html#delete--{db}
+    public function remove() {
+        return (true === $this->client->delete('/'. $this->name)->getData('ok'));
+    }
+
+    // http://docs.couchdb.org/en/1.5.1/api/database/bulk-api.html#get--{db}-_all_docs
+    public function getAllDocuments(array $query = null) {
+        $data = $this->client->get('/'. $this->name .'/_all_docs', $query)->getData();
+        pre($data,1);;
+    }
 }
