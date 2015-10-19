681550a665109a6371fe207357e43cf6dc704a40
Kerem Gunes <qeremy@gmail.com>
2015-10-19 22:41:39 +0300

    Testing new methods.

--- test/test.php
+++ test/test.php
@@ -31,3 +31,28 @@ $server = new Couch\Object\Server($client);
 // pre($server->getConfig('couchdb', 'uuid'));
 // prd($server->setConfig('couchdb', 'foo', 'the foo!'));
 // prd($server->removeConfig('couchdb', 'foo'));
+
+$database = new Couch\Object\Database($client, 'foo');
+// prd($database->ping());
+// pre($database->info());
+// prd($database->create());
+// prd($database->remove());
+
+// 667b0208441066a0954717b50c0008a9 83b5e0a0b3bd41d9a21cee7ae8000615
+// pre($database->getDocument('667b0208441066a0954717b50c0008a9'));
+// pre($database->getDocumentAll());
+// pre($database->getDocumentAll(null, ['667b0208441066a0954717b50c0008a9','83b5e0a0b3bd41d9a21cee7ae8000615']));
+// $offset = 0; $limit = 1; $skip = 0;
+// pre($database->getDocumentAll(['offset' => $offset, 'limit' => $limit, 'skip' => $skip]));
+// pre($skip = Couch\Util\Util::getSkip($offset, $limit));
+// ++$offset; // and continue
+
+
+// $document = new Couch\Object\Document();
+// $document->test = 'the test 2';
+// pre($database->createDocument($document));
+// pre($database->createDocument(['test' => 'test 3']));
+// pre($database->createDocumentAll([
+//     ['test' => 'test 4'],
+//     new Couch\Object\Document(null, null, ['test' => 'the test 5']),
+// ]));
